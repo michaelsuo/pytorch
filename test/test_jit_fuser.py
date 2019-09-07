22cd510b483991ec100fda96282d1c9f6f188a99
Michael Suo <darthsuo@gmail.com>
2019-09-07 00:02:49 -0700

    Update test_jit_fuser.py

--- test/test_jit_fuser.py
+++ test/test_jit_fuser.py
@@ -34,7 +34,7 @@ class TestFuser(JitTestCase):
             return x.abs() * 2
 
         a = torch.randn(5, device=device)
-        self.assertEqual(func(a), a.abs() * 2)
+        self.assertEqual(func(a), a.abs()*2)
         self.assertAllFused(func.graph_for(a))
 
     @unittest.skipIf(IS_WINDOWS or IS_SANDCASTLE, "NYI: fuser support for Windows or Sandcastle")
